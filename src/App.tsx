92009e4869a108422207cadbee3d52e1770a84fe
Pawfen <sh0n.mishel34@gmail.com>
2021-04-21 21:59:00 +0300

    todo wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,131 @@
 import React from "react";
-import logo from "./logo.svg";
+// import { useForm } from "react-hook-form";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import {
+  TextField,
+  Button,
+  Paper,
+  List,
+  ListItem,
+  ListItemIcon,
+  Checkbox,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+} from "@material-ui/core";
+import { Delete } from "@material-ui/icons";
+
 import "./App.css";
 
+type TodoType = {
+  id: number;
+  task: string;
+  complete: boolean;
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+      maxWidth: 720,
+      backgroundColor: theme.palette.background.paper,
+    },
+  })
+);
+
 function App() {
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   watch,
+  //   formState: { errors },
+  // } = useForm<Inputs>();
+
+  const classes = useStyles();
+  const [toDoList, setTodoList] = React.useState<TodoType[]>([]);
+  const [text, setText] = React.useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.currentTarget.value);
+  };
+  const handleSubmit = () => {
+    let idList = toDoList.map((task) => task.id);
+    let maxId = 0;
+    for (let i = 0; i < idList.length; i++) {
+      if (idList[i] > maxId) {
+        maxId = idList[i];
+      }
+    }
+    setTodoList([...toDoList, { id: maxId + 1, task: text, complete: false }]);
+  };
+  const handleDelete = (id: number) => {
+    let deleted = toDoList.filter((task) => {
+      return task.id !== id;
+    });
+    setTodoList(deleted);
+  };
+  const handleChangeBoolean = (id: number) => {
+    let newArr = toDoList.map((task) =>
+      task.id === id ? { ...task, complete: !task.complete } : task
+    );
+    setTodoList(newArr);
+  };
+  const handleFilter = () => {
+    let filtered = toDoList.filter((task) => {
+      return !task.complete;
+    });
+    setTodoList(filtered);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>test</div>
-      </header>
+      <div className="App-header">
+        <TextField
+          label="Введите тудушу"
+          onChange={handleChange}
+          value={text}
+        />
+        <Button color="primary" onClick={handleSubmit}>
+          Отправить
+        </Button>
+        <Button color="primary" onClick={handleFilter}>
+          Удалить готовые
+        </Button>
+        <Paper>
+          <List className={classes.root}>
+            {toDoList.map((value) => {
+              return (
+                <ListItem
+                  key={value.id}
+                  role={undefined}
+                  dense
+                  button
+                  // onClick={handleToggle(value.id)}
+                >
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={value.complete}
+                      tabIndex={-1}
+                      disableRipple
+                      onChange={() => handleChangeBoolean(value.id)}
+                    />
+                  </ListItemIcon>
+                  <ListItemText primary={`${value.id}: ${value.task}`} />
+                  <ListItemSecondaryAction>
+                    <IconButton
+                      edge="end"
+                      onClick={() => handleDelete(value.id)}
+                    >
+                      <Delete />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Paper>
+      </div>
     </div>
   );
 }
