9b71923d35d66917b7944af3225e43824c4d94fa
Pawfen <sh0n.mishel34@gmail.com>
2021-05-30 18:36:05 +0000

    axios testing, select with rerenders

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [animalFacts, setAnimalFacts] = useState<string[]>([]);
+  const [animalType, setAnimalType] = useState<string>("cat");
+
+  const axiosList = async () => {
+    axios
+      .get(
+        `https://cat-fact.herokuapp.com/facts/random?animal_type=${animalType}&amount=2`
+      )
+      .then((value) => {
+        const arr = [];
+        for (let v of value.data) {
+          arr.push(v.text);
+        }
+        setAnimalFacts(arr);
+      });
+
+    return;
+  };
+
+  useEffect(() => {
+    axiosList();
+  }, [animalType]);
+
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <select
+            onChange={(e) => {
+              setAnimalType(e.target.value);
+            }}
+          >
+            <option value="cat">Cat</option>
+            <option value="horse">Horse</option>
+          </select>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <div>test</div>
+        <div>
+          {
+            <ul>
+              {animalFacts.map((v) => (
+                <li>{v}</li>
+              ))}
+            </ul>
+          }
+        </div>
       </header>
     </div>
   );
